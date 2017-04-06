1d7aaf0cdae9714f200d850e4992baebc0e473f5
haarspalter <haarspalter@gmail.com>
2017-04-06 08:32:49 +0200

    Update HelloWorld.java

--- src/main/java/org/sample/HelloWorld.java
+++ src/main/java/org/sample/HelloWorld.java
@@ -1,9 +1,14 @@
 package org.sample;
 
 public class HelloWorld {
+    private boolean foo = false;
     public static void main(String[] args) {
         String msg = "Hello World!";
         String nullString = null;
         System.out.println(nullString.toString());
+    }
+
+    public boolean getFoo() {
+        return foo;
     }
 }
